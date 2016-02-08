7e64d99fbb14ba747da0dd2460a569b9f83ef844
Janpoem <janpoem@163.com>
2016-02-08 16:59:20 +0800

    以docmen作为演示的项目，demo项目将会删除掉。

---
+++ docmen/src/DocMen/Controller/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * KePHP, Keep PHP easy!
+ */
+
+namespace DocMen\Controller;
+
+use Ke\Web\Controller;
+
+class Index extends Controller
+{
+
+	public function index()
+	{
+		// return
+	}
+}
