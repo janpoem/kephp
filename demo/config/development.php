07225d6ed8fc11fb56a579e481d42682e5a22d21
Janpoem <janpoem@163.com>
2016-02-14 13:18:49 +0800

    调整composer说明

--- demo/config/development.php
+++ demo/config/development.php
@@ -9,7 +9,9 @@ use \Ke\Utils\DocMen\DocMen;
 global $app;
 
 $docs = [
-	new DocMen($app->path('doc/kephp2'), $app->kephp(), 'kephp'),
+	new DocMen($app->path('doc/kephp2'), $app->kephp(), 'kephp', function() {
+		$this->setGenerable(false);
+	}),
 ];
 
 $phpExcelSrc = 'D:\xampp\htdocs\sk-917917\917917.cc\library\PHPExcel';
@@ -27,6 +29,7 @@ if (is_dir($phpExcelSrc)) {
 $agimvcSrc = 'D:\xampp\htdocs\sk-917917\917917.cc\library\agimvc';
 if (is_dir($agimvcSrc)) {
 	$docs[] = new DocMen($app->path('doc/agimvc'), $agimvcSrc, 'agimvc', function () {
+		$this->setGenerable(false);
 		$this->setScannerOptions([
 			\Ke\Utils\DocMen\SourceScanner::OPS_AUTO_IMPORT     => false,
 			\Ke\Utils\DocMen\SourceScanner::OPS_NS_STYLE        => DocMen::NS_STYLE_MIXED,
